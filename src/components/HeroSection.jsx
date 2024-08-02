9eb8263eaf052a62c6a9b52beb819a4000fada4a
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-08-02 15:46:41 +0200

    Front page + some fetch setups

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,51 @@
+import { Container } from "react-bootstrap"
+import Image from "react-bootstrap/Image"
+
 const HeroSection = () => {
-  return <></>
+  return (
+    <>
+      <div>
+        <Image src='/public/assets/background.jpg' alt='red-background-d20' fluid />
+      </div>
+
+      <Container>
+        <div className='mt-2 d-flex flex-column justify-content-center text-center'>
+          <h1>NexusFantasia la tua wiki a portata di dado!</h1>
+          <p>Il portale completamente in italiano per la creazione delle tue schede personaggio di Dnd 5e.</p>
+        </div>
+
+        <div className='card my-3 border border-0'>
+          <div className='row g-0'>
+            <div className='col-md-4'>
+              <img src='/public/assets/party.jpg' className='img-fluid' alt='...' />
+            </div>
+            <div className='col-md-8 d-flex align-items-center text-center'>
+              <div className='card-body'>
+                <h5 className='card-title'>Classi e sottoclassi</h5>
+                <p className='card-text'>
+                  Tutte le informazioni di cui potreste avere bisogno per scegliere la classe giusta per voi!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='card my-3 border border-0'>
+          <div className='row g-0'>
+            <div className='col-md-4'>
+              <img src='/public/assets/races.jpg' className='img-fluid' alt='...' />
+            </div>
+            <div className='col-md-8 d-flex align-items-center text-center'>
+              <div className='card-body'>
+                <h5 className='card-title'>Razze</h5>
+                <p className='card-text'>Puoi essere qualsiasi cosa tu voglia ad ogni momento!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
 }
 
 export default HeroSection
--- src/components/RacesListSection.jsx
+++ src/components/RacesListSection.jsx
@@ -1,4 +1,62 @@
+import { useEffect } from "react"
+
 const RaceListSection = () => {
+  const fetchRaces = () => {
+    fetch("http://localhost:3001/api/races", {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3MjI2MDUxNTcsImV4cCI6MTcyMzIwOTk1Nywic3ViIjoiY2Q4MmNlZGUtMmJmMC00MWIzLThlZDEtZTdhOTdiODVhZjA4In0.ltL10zyALqFZ3rse-vVs34Zjl5dyggATz9MT6mSajqJBs8BhFg5-Y9l_NWKEvseL",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          if (response.status === 400) {
+            throw new Error("400: Bad Request")
+          }
+          if (response.status === 401) {
+            throw new Error("401: Unauthorized")
+          }
+          if (response.status === 402) {
+            throw new Error("402: Payment Required")
+          }
+          if (response.status === 403) {
+            throw new Error("403: Forbidden")
+          }
+          if (response.status === 404) {
+            throw new Error("404: Not Found")
+          }
+          if (response.status === 405) {
+            throw new Error("405: Method Not Allowed")
+          }
+          if (response.status === 406) {
+            throw new Error("406: Not Acceptable")
+          }
+          if (response.status === 407) {
+            throw new Error("407: Proxy Authentication Required")
+          }
+          if (response.status === 408) {
+            throw new Error("408: Request Timeout")
+          }
+          if (response.status === 500) {
+            throw new Error("500: Server Error")
+          }
+
+          throw new Error("Generic Fetch Error")
+        }
+      })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    fetchRaces()
+  }, [])
   return <></>
 }
 
